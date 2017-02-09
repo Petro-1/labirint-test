76a93cbc151297ab19fe6738f54ac2cde3298792
Ivan <ivan.kopenkov@gmail.com>
2017-02-09 13:17:21 +0300

    psr2

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
-<?php 
+<?php
 
-class Test {
+class Test
+{
 
-	public function __set($name, $value) 
-	    {
-	    	$trace = debug_backtrace();
-	    	if(!isset($trace[1]) || $trace[1]['object'] != $trace[0]['object']) {
-	    		try {
-	    		    throw new Exception("Попытка присвоения значения readonly свойству\n\n");
-	    		} catch(Exception $e) {
-	    		    echo $e->getMessage();
-	    		}
-	    	} else {	    		
-	    		$this->$name = $value;
-	    	}	        
-	    }
+    public function __set($name, $value)
+    {
+        $trace = debug_backtrace();
+        if (!isset($trace[1]) || $trace[1]['object'] != $trace[0]['object']) {
+            try {
+                throw new Exception("Попытка присвоения значения readonly свойству\n\n");
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+        } else {
+            $this->$name = $value;
+        }
+    }
 
-    public function __get($name) 
+    public function __get($name)
     {
         if (isset($this->$name)) {
             return $this->$name;
         } else {
-        	try {
-        	    throw new Exception("Попытка получения значения несуществующего свойства\n\n");
-        	} catch(Exception $e) {
-        	    echo $e->getMessage();
-        	}
+            try {
+                throw new Exception("Попытка получения значения несуществующего свойства\n\n");
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
         }
     }
 
-    public function __isset($name) 
-    {        
+    public function __isset($name)
+    {
         return isset($this->$name);
     }
 }
@@ -41,21 +42,21 @@ class Test {
  */
 class Obj extends Test
 {
-	/**
-	 * readonly свойство
-	 * @var string 
-	 */
+    /**
+     * readonly свойство
+     * @var string
+     */
     protected $protected = 'protected';
 
     /**
      * доступное для изменения свойство
-     * @var string 
+     * @var string
      */
     public $public = 'public';
 
     /**
      * еще одно доступное для изменения свойство
-     * @var string 
+     * @var string
      */
     public $another = 'another public';
 }
@@ -79,6 +80,3 @@ echo $obj->another . "\n\n";
 var_dump(isset($obj->nonexist));
 
 echo $obj->nonexist;
-
-
-?>
